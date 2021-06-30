f619c6eb84866386afc5ac4781bd4efd32224952
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2021-06-30 03:50:38 -0300

    fix 2

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(( request, response ) => {
   });
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
